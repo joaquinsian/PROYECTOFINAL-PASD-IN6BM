7128c70874e249ad14871a4d087a406169fe465c
lmorales2019047 <lmorales-2019047@kinal.edu.gt>
2021-07-23 15:22:10 -0600

    Se creó diseño base para todas las vistas

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
