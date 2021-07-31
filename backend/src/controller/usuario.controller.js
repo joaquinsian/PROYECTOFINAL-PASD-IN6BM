93e850bdb6108aac2f3ca988e66cde3c92bc5749
lmorales2019047 <lmorales-2019047@kinal.edu.gt>
2021-07-30 18:06:31 -0600

    Se agregó restricción de roles en Mi Usuario

--- backend/src/controller/usuario.controller.js
+++ backend/src/controller/usuario.controller.js
@@ -3,6 +3,7 @@ const Usuario = require('../models/usuario.model')
 const Datos_Doctor = require('../models/datos_doctor.model')
 const Rel_Doc_User = require('../models/rel_doc_user.model')
 const bcrypt = require('bcrypt-nodejs')
+const jwt = require('jwt-simple')
 
 //Función para registro
 async function registro(req, res) {
@@ -46,8 +47,8 @@ async function registro(req, res) {
 }
 
 async function obtenerIdentidad(req, res) {
-    const tokenuser = req.headers.authorization;
-    res.json(tokenuser)
+    let x = jwt.decode(req.headers["authorization"], "PASD");
+    res.json(x)
 }
 
 async function obtenerUsuarios(req, res) {
--- frontend/src/app/components/my-user/my-user.component.ts
+++ frontend/src/app/components/my-user/my-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { LoginService } from 'src/app/services/login/login.service';
+
 
 @Component({
   selector: 'app-my-user',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-user.component.css']
 })
 export class MyUserComponent implements OnInit {
+  public role="";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private titleService: Title,public loginService:LoginService) {
+    this.titleService.setTitle("Mi Usuario");
   }
 
+  ngOnInit(): void {
+    this.getIdentidad();
+  }
+
+  getIdentidad(){
+    this.loginService.getIdentity().subscribe(
+      res => this.role = res.rol,
+      err => console.error(err)
+    );
+    
+  }
 }
